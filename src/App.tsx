7f3f5b33af73290ecabea0eacdf68ed0466b1830
aydengibon-droid <aydengibon@gmail.com>
2025-09-26 12:58:09 -0400

    3 Pictures of Legends

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 
 function App() {
-  
+
   return (
-    <>
-      <div></div>
-      <div></div>
-    </>
+    <div className="flex flex-col items-center ">
+      <div className="mb-8">
+        <h1 className="text-4xl"> My Blog</h1>
+      </div>{/* First row */}
+
+      <div>
+        <div>
+          <img src="bea3491915571d34a026753f4a872000.jpg" alt="Photo of Elba" />
+          <h1>Post 1 Headline</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Tuesday,October 2025</p>
+          <button>Read More</button>
+        </div> {/* first column */}
+      </div>{/* second row */}
+
+    </div>
   )
 }
 
